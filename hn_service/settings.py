9180cd24fb152612949856d4041d07875f3dba83
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-07-09 19:04:28 +0100

    Add setup for render deployment

--- hn_service/settings.py
+++ hn_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q%egq&59c2*-+^ys0!fhf))mg)9#i(5+6%(lu*q)ogi&)1kc*g'
+key = 'django-insecure-q%egq&59c2*-+^ys0!fhf))mg)9#i(5+6%(lu*q)ogi&)1kc*g'
+SECRET_KEY = os.environ.get('SECRET_KEY', default=key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
