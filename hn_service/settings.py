4aa98c8fbe47e4d9bf1bcc1d4583de8c050882f6
Osakpolor Obaseki <obasekiosa@gmail.com>
2023-07-12 17:08:07 +0100

    Update allowed hosts and debug mode settings

--- hn_service/settings.py
+++ hn_service/settings.py
@@ -25,14 +25,13 @@ key = 'django-insecure-q%egq&59c2*-+^ys0!fhf))mg)9#i(5+6%(lu*q)ogi&)1kc*g'
 SECRET_KEY = os.environ.get('SECRET_KEY', default=key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'RENDER' not in os.environ
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
-# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-# if RENDER_EXTERNAL_HOSTNAME:
-#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
